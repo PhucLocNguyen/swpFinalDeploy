e3f427a601d20e76c448fcca9a3156f3ea6f036a
PhucLocNguyen <loc19992003bca@gmail.com>
2024-07-06 11:34:48 +0700

    fix instance

--- src/api/instance.jsx
+++ src/api/instance.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: 'https://app-swp391-sp24-dev-001.azurewebsites.net/api/ '
+   baseURL: 'https://app-swp391-sp24-dev-001.azurewebsites.net/api/'
 })
