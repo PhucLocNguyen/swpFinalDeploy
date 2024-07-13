14fc6a90db38d3501f93a18bfc749cf6dc964886
PhucLocNguyen <loc19992003bca@gmail.com>
2024-07-13 10:51:17 +0700

    fix logic place order

--- src/component/requirements/Create/FirstStep.jsx
+++ src/component/requirements/Create/FirstStep.jsx
@@ -1,57 +1,51 @@
 import { useContext, useEffect, useState } from "react";
 import { multiStepContext } from "./StepContext";
 import { CustomButton } from "../../home/Home";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import "./style.css";
 import { TextField } from "@mui/material";
 import { FetchApiMaterial } from "../../../api/Requirements/FetchApiMaterial";
+
 function FirstStep({ handleCompleteStep }) {
-  const { currentStep, setCurrentStep, requirementData, setRequirementData, designRuleState , animate} =
+  const { currentStep, setCurrentStep, requirementData, setRequirementData, designRuleState, animate } =
     useContext(multiStepContext);
   const [isAllowed, setAllowed] = useState(false);
   const [data, setData] = useState({
-    material: requirementData.material ==0 ? null :requirementData.material,
-    size: requirementData.size ==0? null :requirementData.size,
+    material: requirementData.material === 0 ? null : requirementData.material,
+    size: requirementData.size === 0 ? null : requirementData.size,
   });
-  
   const [materialList, setMaterialList] = useState([]);
 
-  useEffect(()=>{
-    const materialApi = FetchApiMaterial().then((res)=>{
-      setMaterialList([...res]);
-    })
-    
-  },[])
   useEffect(() => {
-    var output = true;
+    const fetchMaterials = async () => {
+      const res = await FetchApiMaterial();
+      setMaterialList([...res]);
+    };
+    fetchMaterials();
+  }, []);
+
+  useEffect(() => {
+    let output = true;
     Object.entries(data).forEach(([key, value]) => {
-      if (value == -1) {
-        output = false;
-        setAllowed(false);
-        return;
-      }
-      if(data.material==null){
-        output = false;
-        setAllowed(false);
-        return;
-      }
-      if(key==="size" && !(value>=designRuleState.MinSizeJewellery && value<=designRuleState.MaxSizeJewellery))
-      {
+      if (value === -1 || (key === "size" && !(value >= designRuleState.MinSizeJewellery && value <= designRuleState.MaxSizeJewellery)) || data.material === null) {
         output = false;
         setAllowed(false);
         return;
       }
     });
     if (output) {
-      setAllowed(true)};
-  }, [data]);
-  function NextStep(){
-    if(isAllowed){
-      handleCompleteStep(currentStep-1);
-      setCurrentStep(currentStep+1);
-      setRequirementData({...requirementData,...data })
+      setAllowed(true);
+    }
+  }, [data, designRuleState.MinSizeJewellery, designRuleState.MaxSizeJewellery]);
+
+  function NextStep() {
+    if (isAllowed) {
+      handleCompleteStep(currentStep - 1);
+      setCurrentStep(currentStep + 1);
+      setRequirementData({ ...requirementData, ...data });
     }
   }
+
   const HandleChangeData = (e) => {
     const { name, value } = e.target;
     setData({
@@ -59,28 +53,47 @@ function FirstStep({ handleCompleteStep }) {
       [name]: value,
     });
   };
-  
+
   return (
-    <motion.div initial={{ opacity: 0, x:50 }} whileInView={{opacity:1, x:0}} className="mx-16">
-      <h2 className="text-[24px] mb-1 mt-3">Matterial </h2>
-      <div className="grid grid-cols-5  mb-[30px] gap-4">
-      {materialList.map((item,index)=>{
-          return <label key={item.name+index} htmlFor={"material-"+index} className="rounded-md border border-[#646464] cursor-pointer" >
-          <div className="shadow-lg relative h-[100px]">
-          <input type="radio" name="material" id={"material-"+index} defaultChecked={requirementData.material==item.materialId} value={item.materialId} className="hidden peer" onChange={HandleChangeData} />
-          <span className="w-[20px] h-[20px] mb-[50px] top-1 left-1 inline-block border-[2px] border-[#e3e3e3] rounded-full relative z-10 peer-checked:bg-primary checkedBoxFormat peer-checked:border-[#3057d5] peer-checked:scale-110 peer-checked:bg-[#3057d5] peer-checked:before:opacity-100"></span>
-          <img src={item.image} className="rounded-md w-full absolute top-0 h-[80px]"/>
-          <p className="text-center">{item.name}</p>
-          </div>
-      </label>
-        })}
+    <motion.div initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} className="mx-16">
+      <h2 className="text-[24px] mb-1 mt-3">Material</h2>
+      <div className="grid grid-cols-5 mb-[30px] gap-4">
+        {materialList.map((item, index) => (
+          <label key={item.name + index} htmlFor={"material-" + index} className="rounded-md border border-[#646464] cursor-pointer">
+            <div className="shadow-lg relative h-[100px]">
+              <input
+                type="radio"
+                name="material"
+                id={"material-" + index}
+                defaultChecked={requirementData.material === item.materialId}
+                value={item.materialId}
+                className="hidden peer"
+                onChange={HandleChangeData}
+              />
+              <span className="w-[20px] h-[20px] mb-[50px] top-1 left-1 inline-block border-[2px] border-[#e3e3e3] rounded-full relative z-10 peer-checked:bg-primary checkedBoxFormat peer-checked:border-[#3057d5] peer-checked:scale-110 peer-checked:bg-[#3057d5] peer-checked:before:opacity-100"></span>
+              <img src={item.image} className="rounded-md w-full absolute top-0 h-[80px]" />
+              <p className="text-center">{item.name}</p>
+            </div>
+          </label>
+        ))}
       </div>
       <h2 className="text-[24px] mb-1 mt-3">Size</h2>
       <div className="relative mb-5">
-      <TextField type="number" name="size" id="" value={data.size} helperText={ (typeof(data.size)==='string' && !(data.size>=designRuleState.MinSizeJewellery && data.size<=designRuleState.MaxSizeJewellery )) ?`The size must in range of ${designRuleState.MinSizeJewellery} to ${designRuleState.MaxSizeJewellery}`: null} error={(typeof(data.size)==='string' && !(data.size>=designRuleState.MinSizeJewellery && data.size<=designRuleState.MaxSizeJewellery ) )} onChange={HandleChangeData} placeholder={`Enter your size from ${designRuleState.MinSizeJewellery} to ${designRuleState.MaxSizeJewellery}.`} className="w-full" />
-      <span className="checkedBoxFormat absolute"></span>
+        <TextField
+          type="number"
+          name="size"
+          id=""
+          value={data.size}
+          helperText={typeof data.size === "string" && !(data.size >= designRuleState.MinSizeJewellery && data.size <= designRuleState.MaxSizeJewellery)
+            ? `The size must be in range of ${designRuleState.MinSizeJewellery} to ${designRuleState.MaxSizeJewellery}`
+            : null}
+          error={typeof data.size === "string" && !(data.size >= designRuleState.MinSizeJewellery && data.size <= designRuleState.MaxSizeJewellery)}
+          onChange={HandleChangeData}
+          placeholder={`Enter your size from ${designRuleState.MinSizeJewellery} to ${designRuleState.MaxSizeJewellery}.`}
+          className="w-full"
+        />
+        <span className="checkedBoxFormat absolute"></span>
       </div>
-      
       <CustomButton
         variant="contained"
         disabled={!isAllowed}
@@ -92,9 +105,9 @@ function FirstStep({ handleCompleteStep }) {
           fontSize: "1rem",
           fontWeight: 400,
           lineHeight: "1.5rem",
-          display:"flex",
-          justifyContent:"justify-center",
-          width:"100%"
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
         }}
         onClick={NextStep}
       >
